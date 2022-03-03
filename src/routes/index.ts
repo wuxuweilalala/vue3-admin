0190ba18ac5793de2dd36bc29ce3bdd1c6c3a79e
wuxuwei <2509165479@qq.com>
2022-03-03 21:15:24 +0800

    feat: add vitePages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,23 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Layout from '@/layout/index.vue'
+import children from "pages-generated"; 
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: () => import(/* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue'),
-        meta: {
-          title: 'Dashboard'
-        }
-      }
-    ]
+    children
+    // children: [
+    //   {
+    //     path: 'dashboard',
+    //     name: 'Dashboard',
+    //     component: () => import(/* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue'),
+    //     meta: {
+    //       title: 'Dashboard'
+    //     }
+    //   }
+    // ]
   }
 ]
 
