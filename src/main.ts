1ece7e606d5ac85f7a41a9de0d1169eac2dac168
wuxuwei <2509165479@qq.com>
2022-03-03 21:06:39 +0800

    feat:add svgIcon component

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './routes'
 import store from './store'
+import 'normalize.css/normalize.css'
 import '@/styles/index.scss'
+import 'virtual:svg-icons-register'
+import initSvgIcon from '@/icons/index'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(store)
 app.use(router)
+app.use(initSvgIcon)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const { resolve } = require('path')
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path, { resolve } from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/icons/svg")],
+      symbolId: "icon-[dir]-[name]",
+      inject: "body-last",
+      customDomId: "__svg__icons__dom__",
+    }),
+  ],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: resolve(__dirname, 'src')
-      }
-    ]
-  }
-})
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
