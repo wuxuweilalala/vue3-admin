382b7aef52bf2d7cc810d320622216cbf8a10110
wuxuwei <2509165479@qq.com>
2022-03-03 21:20:05 +0800

    feat: add elementPlus plugin

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-
-import App from './App.vue'
-import router from './routes'
+import router from './routes/index';
 import store from './store'
+import App from './App.vue'
 import 'normalize.css/normalize.css'
 import '@/styles/index.scss'
 import 'virtual:svg-icons-register'
 import initSvgIcon from '@/icons/index'
+// element-plus
+import installElementPlus, { Size } from './plugins/element'
+// 挂载到vue实例上
+import { ElMessageBox, ElMessage, ElNotification } from 'element-plus'
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(store)
-app.use(router)
-app.use(initSvgIcon)
-app.mount('#app')
+createApp(App)
+    .use(store)
+    .use(installElementPlus)
+    .use(initSvgIcon)
+    .use(router)
+    .mount('#app')
+
+
+// vue实例上挂载属性类型声明
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $message: typeof ElMessage;
+    $notify: typeof ElNotification;
+    $confirm: typeof ElMessageBox.confirm;
+    $alert: typeof ElMessageBox.alert;
+    $prompt: typeof ElMessageBox.prompt;
+    $ELEMENT: {
+      size:Size
+    }
+  }
+}
